82fb27d6eccacb8bc44733f31e86a6696d9e142f
devlikepro <waha@devlike.pro>
2024-11-24 16:56:15 +0700

    init meow

--- src/main.go
+++ src/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	pb "github.com/devlikeapro/noweb2/proto"
 	"github.com/devlikeapro/noweb2/server"
-	"github.com/mdp/qrterminal/v3"
-	"go.mau.fi/whatsmeow"
-	"go.mau.fi/whatsmeow/store/sqlstore"
+	"github.com/devlikeapro/noweb2/service"
 	"go.mau.fi/whatsmeow/types/events"
-	waLog "go.mau.fi/whatsmeow/util/log"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -17,48 +13,6 @@ import (
 
 	_ "github.com/mattn/go-sqlite3" // Import the SQLite drive
 )
-
-func BuildClient() *whatsmeow.Client {
-	dbLog := waLog.Stdout("Database", "DEBUG", true)
-	container, err := sqlstore.New("sqlite3", "file:.sessions/noweb2.db?_foreign_keys=on", dbLog)
-	if err != nil {
-		panic(err)
-	}
-	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
-	deviceStore, err := container.GetFirstDevice()
-	if err != nil {
-		panic(err)
-	}
-	clientLog := waLog.Stdout("Client", "DEBUG", true)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
-
-	if client.Store.ID == nil {
-		// No ID stored, new login
-		qrChan, _ := client.GetQRChannel(context.Background())
-		err = client.Connect()
-		if err != nil {
-			panic(err)
-		}
-		for evt := range qrChan {
-			if evt.Event == "code" {
-				// Render the QR code here
-				// e.g. qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
-				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
-				fmt.Println("QR code:", evt.Code)
-				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
-			} else {
-				fmt.Println("Login event:", evt.Event)
-			}
-		}
-	} else {
-		// Already logged in, just connect
-		err = client.Connect()
-		if err != nil {
-			panic(err)
-		}
-	}
-	return client
-}
 
 func eventHandler(evt interface{}) {
 	switch v := evt.(type) {
@@ -82,14 +36,14 @@ func main() {
 	grpcServer := grpc.NewServer()
 
 	// Register the TimestampService with the server
-	client := BuildClient()
+	client := service.BuildClient()
 	client.AddEventHandler(eventHandler)
 
 	pb.RegisterMessageServiceServer(grpcServer, &server.Server{
 		Client: client,
 	})
 
-	log.Println("Server is listening on port 50051")
+	log.Println("Server is listening on", socket)
 
 	// Start the server
 	if err := grpcServer.Serve(listener); err != nil {
