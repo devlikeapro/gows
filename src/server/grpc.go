39ba4dcceeeb6e6fab172ee71ad1c09deea4db16
devlikepro <waha@devlike.pro>
2025-01-16 16:36:32 +0700

    compile-time asserts

--- src/server/grpc.go
+++ src/server/grpc.go
@@ -9,9 +9,15 @@ import (
 	"sync"
 )
 
+// assert that Server implements pb.MessageServiceServer
+var _ pb.MessageServiceServer = (*Server)(nil)
+
+// assert that Server implements pb.EventStreamServer
+var _ pb.EventStreamServer = (*Server)(nil)
+
 type Server struct {
-	pb.UnimplementedMessageServiceServer
-	pb.UnimplementedEventStreamServer
+	pb.UnsafeMessageServiceServer
+	pb.UnsafeEventStreamServer
 	Sm  *gows.SessionManager
 	log waLog.Logger
 
